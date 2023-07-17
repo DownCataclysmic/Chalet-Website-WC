3db80828417c1c73dab9950d8e35c031363d29c4
Danny Maksoud <dannymaksoud@gmail.com>
2023-07-17 16:01:35 +0100

    Initial commit

---
+++ my-app/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import Hero from '@/components/Hero'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Castleman Retreats</title>
+      </Head>
+      <Hero />
+    </div>
+  )
+}
