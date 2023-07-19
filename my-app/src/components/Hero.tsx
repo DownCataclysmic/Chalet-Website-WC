7f83bd21b08102e5438c70790b128f560215c193
Danny Maksoud <dannymaksoud@gmail.com>
2023-07-19 19:19:17 +0100

    Changed the Hero component so we can say if we want the button present or not. The button is not present on the About Page

--- my-app/src/components/Hero.tsx
+++ my-app/src/components/Hero.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 interface HeroProps {
     heading: string;
     message: string;
+    showButton?: boolean; //Button may or may not show up depending on the page
 }
 
-const Hero: React.FC<HeroProps> = ({heading, message}) => {
+const Hero: React.FC<HeroProps> = ({heading, message, showButton}) => {
     return (
         <div className='flex items-center justify-center h-screen mb-[12] bg-fixed bg-center bg-cover custom-img'>
             {/*Overlay*/}
@@ -13,7 +14,9 @@ const Hero: React.FC<HeroProps> = ({heading, message}) => {
             <div className='p-5 text-white z-[2] mt-[-10rem]'>
                 <h2 className='text-5xl font-bold'>{heading}</h2>
                 <p className='py-5 text-xl'>{message}</p>
+                {showButton && (
                 <button className='px-8 py-2 border'>Book</button>
+                )}
             </div>
         </div>
     );
--- my-app/src/pages/about.tsx
+++ my-app/src/pages/about.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Hero from '../components/Hero'
 
-const About = () => {
+const about = () => {
   return (
-    <div>
+    <div className="items-center">
       <Hero heading='About Us' message="If you don't like it then tough..."/>
+      <h1 className='text-4xl font-bold text-center p-4'>About Us</h1>
+      <p className="text-lg text-gray-600 text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae tincidunt turpis. Vestibulum lacinia
+        ligula et commodo viverra. Proin quis maximus orci.
+      </p>
+      <div>
+        <img
+          src="/about-image.jpg"
+          alt="About Us"
+          className="w-full rounded-lg shadow-lg"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default about
